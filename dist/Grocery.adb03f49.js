30ed478a9d264d4773a7a960228ae0295c03405c
salman <salmanparay7@gmail.com>
2024-04-04 22:14:08 +0530

    testing-progress

---
+++ dist/Grocery.adb03f49.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire3f7c;(0,e.register)("63M6l",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var t=e("ayMG0"),l=()=>(0,t.jsx)("h1",{children:"The Grocery Store !!! Which has lot of child components"})});
+//# sourceMappingURL=Grocery.adb03f49.js.map
